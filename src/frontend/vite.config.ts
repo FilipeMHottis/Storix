3c24208ce345fbd3b8ab8bdddc5175911e4c8ae7
Filipe Marques Hottis <lipehottis@gmail.com>
2024-01-11 13:54:58 -0300

    feat: chaging frontend to vite and add lint in backend and frontend

---
+++ src/frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const PORT = Number(process.env.PORT) || 8080
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  preview: {
+    port: PORT,
+    host: true
+  },
+  server: {
+    port: PORT,
+    host: true,
+    origin: `http://0.0.0.0:${PORT}`,
+  },
+  plugins: [react()],
+})
